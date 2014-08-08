e7f3cb0fd6f884346f3d182974d55ae79a8d346e
Kai Kraft <kraftykai@gmail.com>
2014-08-08 13:21:59 -0700

    Staging

--- test/index.js
+++ test/index.js
@@ -2,20 +2,15 @@
 var should = require('chai').should(),
 	bridge = require('../index'),
 	getIceServers = bridge.getIceServers,
-	name = 'default',
 	return_value;
+	name = 'default',
 	
-function run_test() {
-	describe('#return_value', function() {
-		it('is a json object containing iceServers', function() {
-			return_value.should.have.property('noodles');
+describe('#getIceServers(' + name + ')', function() {
+	it('should pass a json object of iceservers to the callback', function(done) {
+		getIceServers(name, function (ice_servers) {
+			console.log(ice_servers);
 		});
 	});
-}
-
-getIceServers(name, function(ice_servers) {
-	return_value = ice_servers;
-	run_test;
 });
 	
 console.log("WARNING: dev_config.json MUST CONTAIN VALID API KEY FOR TESTS TO PASS!");
