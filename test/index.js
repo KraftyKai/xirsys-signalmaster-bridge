ba48f06c34e4f53ad38f234074f2c220c1da72b8
Kai Kraft <kraftykai@gmail.com>
2014-08-08 12:25:36 -0700

    Staging

--- test/index.js
+++ test/index.js
@@ -1,24 +1,22 @@
 // index.js for tests
 var should = require('chai').should(),
 	bridge = require('../index'),
-	getIceServers = bridge.getIceServers;
-	name = 'default';
+	getIceServers = bridge.getIceServers,
+	name = 'default',
+	return_value;
+	
+function run_test() {
+	describe('#return_value', function() {
+		it('is a json object containing iceServers', function() {
+			return_value.should.have.property('noodles');
+		});
+	});
+}
+
+getIceServers(name, function(ice_servers) {
+	return_value = ice_servers;
+	run_test;
+});
 	
 console.log("WARNING: dev_config.json MUST CONTAIN VALID API KEY FOR TESTS TO PASS!");
-
-describe('#getIceServers', function() {
-	
-	it('returns IceServer configuration values JSON object', function() {
-		getIceServers(name, function(ice_servers) {
-			ice_servers.should.have.property('iceServers');
-		});
-	});
-	
-	it('Contains at least one url', function() {
-		getIceServers(name, function(ice_servers) {
-			ice_servers.should.have.property('belch');
-		});
-	});
-	
-});
 
